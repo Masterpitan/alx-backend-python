c2b62b4533ebe7b8dc2408abb9a9fd4b925df377
Masterpitan <akoredeadetunji93@gmail.com>
2025-07-05 05:48:02 +0100

    Running batch processing

---
+++ python-generators-0x00/1-batch_streaming.py
@@ -0,0 +1,47 @@
+import mysql.connector
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_USER = os.getenv("DB_USER", "root")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "")
+DB_NAME = os.getenv("DB_NAME", "ALX_prodev")
+
+def stream_users_in_batches(batch_size):
+    conn = mysql.connector.connect(
+        host=DB_HOST,
+        user=DB_USER,
+        password=DB_PASSWORD,
+        database=DB_NAME
+    )
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM user_data")
+
+    batch = []
+    for row in cursor:
+        batch.append(row)
+        if len(batch) == batch_size:
+            yield batch
+            batch = []
+
+    if batch:
+        yield batch  # yield remaining rows
+
+    cursor.close()
+    conn.close()
+
+def batch_processing(batch_size):
+    for batch in stream_users_in_batches(batch_size):  # Loop 1
+        for user in batch:
+            if user['age'] > 25:
+                filtered = user  # Loop 2 (list comp)
+        yield filtered  # Yield only users over age 25
+
+# Optional test run
+if __name__ == "__main__":
+    for filtered_batch in batch_processing(5):  # Loop 3
+        print("Filtered batch:")
+        for user in filtered_batch:
+            print(user)
