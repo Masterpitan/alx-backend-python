c34857d2f89f4c7ccd4aab2971896879bfd4be7d
Masterpitan <akoredeadetunji93@gmail.com>
2025-07-07 11:27:27 +0100

    Task 0. Logging database Queries

---
+++ python-decorators-0x01/0-log_queries.py
@@ -0,0 +1,28 @@
+import sqlite3
+import functools
+
+def log_queries(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        if 'query' in kwargs:
+            query = kwargs.get['query']
+        elif args:
+            query = args[0]
+        else:
+            query = None
+        if query:
+            print(f"SQL Query: {query}")
+        else:
+            print(f"No query found")
+        return func(*args, **kwargs)
+    return wrapper
+@log_queries
+def fetch_all_users(query):
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    cursor.execute(query)
+    results = cursor.fetchall()
+    conn.close
+    return results
+# Fetch users while logging the query
+users = fetch_all_users("query=SELECT * FROM users")
