9f0d026e4c6f326ce2627a1d741c86d4d69328ef
Masterpitan <akoredeadetunji93@gmail.com>
2025-07-07 14:10:08 +0100

    Task 3. Using Decorators to retry database queries

---
+++ python-decorators-0x01/3-retry_on_failure.py
@@ -0,0 +1,46 @@
+import time
+import sqlite3
+import functools
+
+# My db_connection function
+def with_db_connection(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        conn = sqlite3.connect('users.db')
+        try:
+            return func(conn, *args, **kwargs)
+        finally:
+            conn.close()
+    return wrapper
+
+# Attempts function
+def retry_on_failure(retries=3, delay=2):
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            for attempt in range(1, retries + 1):
+                try:
+                    return func(*args, **kwargs)
+                except Exception as e:
+                    print(f"[Attempt {attempt}] Failed with error: {e}")
+                    if attempt == retries:
+                        print("You have reached the maximum tries. Raising error.")
+                        raise
+                    else:
+                        print(f"Retrying in {delay} seconds...")
+                        time.sleep(delay)
+        return wrapper
+    return decorator
+
+@with_db_connection
+@retry_on_failure(retries=3, delay=1)
+
+def fetch_users_with_retry(conn):
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM users")
+    return cursor.fetchall()
+
+#### attempt to fetch users with automatic retry on failure
+
+users = fetch_users_with_retry()
+print(users)
