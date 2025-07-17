85b858df1e84b2b04fc85196052f17213cd3d6c5
Masterpitan <akoredeadetunji93@gmail.com>
2025-07-17 16:50:25 +0100

    Final project

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -85,3 +85,11 @@ class TestGithubOrgClient(unittest.TestCase):
         """Test the has_license static method."""
         result = GithubOrgClient.has_license(repo, license_key)
         self.assertEqual(result, expected)
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False),
+    ])
+    def test_has_license(self, repo, license_key, expected):
+        """Test the has_license method"""
+        client = GithubOrgClient("test_org")
+        self.assertEqual(client.has_license(repo, license_key), expected)
