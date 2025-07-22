a919291df475e94160b34a93692d040ba551c0c5
Masterpitan <akoredeadetunji93@gmail.com>
2025-07-22 13:32:42 +0100

    Configuring paths for urls and views

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -123,6 +123,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'chats.User'
+
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
