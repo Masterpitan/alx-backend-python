8e20cb81740fed6fc37b661635f7185116eafe06
Masterpitan <akoredeadetunji93@gmail.com>
2025-07-22 13:24:28 +0100

    Configuring views models

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from .models import Conversation, Message
+from .serializers import ConversationSerializer, MessageSerializer, CreateMessageSerializer
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+
+class ConversationViewSet(viewsets.ModelViewSet):
+    queryset = Conversation.objects.all()
+    serializer_class = ConversationSerializer
+    permission_classes = [IsAuthenticated]
+
+    def create(self, request, *args, **kwargs):
+        participants = request.data.get('participants', [])
+        if not participants or len(participants) < 2:
+            return Response(
+                {"error": "A conversation must have at least two participants."},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        conversation = Conversation.objects.create()
+        conversation.participants.set(participants)
+        serializer = self.get_serializer(conversation)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class MessageViewSet(viewsets.ModelViewSet):
+    queryset = Message.objects.all()
+    permission_classes = [IsAuthenticated]
+
+    def get_serializer_class(self):
+        if self.action == 'create':
+            return CreateMessageSerializer
+        else:
+            return MessageSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(sender=self.request.user)
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        conversation_id = request.data.get("conversation")
+        conversation = get_object_or_404(Conversation, pk=conversation_id)
+
+        message = serializer.save(sender=request.user, conversation=conversation)
+        return Response(MessageSerializer(message).data, status=status.HTTP_201_CREATED)
