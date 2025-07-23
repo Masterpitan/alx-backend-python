e0f826e209d02a8b3c964b17bfefc153e36030f3
Masterpitan <akoredeadetunji93@gmail.com>
2025-07-23 11:32:49 +0100

    committing new fix

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -132,9 +132,15 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication'
         'rest_framework_simplejwt.authentication.JWTAuthentication'
     ],
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend',
     ],
+
+    'AUTHENTICATION_BACKENDS': [
+    'chats.auth.EmailAuthBackend',
+    'django.contrib.auth.backends.ModelBackend',
+    ]
 }
