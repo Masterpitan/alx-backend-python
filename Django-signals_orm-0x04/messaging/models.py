6eb7126b7bbb1112521bc76b001c7472eaf8082d
Adepitan <akoredeadetunji93@gmail.com>
2025-07-31 11:05:39 +0100

    Adding edited_by

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -7,6 +7,7 @@ class Message(models.Model):
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
     edited = models.BooleanField(default=False)
+    edited_by = models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL, related_name='eidted_messages')
 
     def __str__(self):
         return f"From {self.sender} to {self.receiver} at {self.timestamp}"
