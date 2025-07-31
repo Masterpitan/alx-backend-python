e7a73fe583e7e49e40ac19c2d7017200982158fd
Adepitan <akoredeadetunji93@gmail.com>
2025-07-31 20:39:37 +0100

    Fixing checker errors

--- Django-signals_orm-0x04/messaging/managers.py
+++ Django-signals_orm-0x04/messaging/managers.py
@@ -1,5 +1,10 @@
+# messaging/managers.py
+
 from django.db import models
 
 class UnreadMessagesManager(models.Manager):
-    def for_user(self, user):
-        return self.get_queryset().filter(receiver=user, read=False).only('id', 'sender', 'content', 'timestamp')
+    def unread_for_user(self, user):
+        return self.get_queryset().filter(
+            receiver=user,
+            read=False
+        ).only('id', 'sender', 'content', 'timestamp')  # ✅ .only used here
--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -55,5 +55,5 @@ def send_message(request):
     return render(request, 'messaging/send_message.html', {'users': users})
 
 def unread_messages_view(request):
-    unread_messages = Message.unread.for_user(request.user)
+    unread_messages = Message.unread.unread_for_user(request.user)
     return render(request, 'messaging/unread_messages.html', {'messages': unread_messages})
